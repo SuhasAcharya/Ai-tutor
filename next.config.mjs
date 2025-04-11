8265f1c3666010db007f3950ae8544b01e217a10
Suhas Acharya <suhasacharya2000@gmail.com>
2025-04-12 01:35:06 +0530

    build issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Add any other configurations you need (if any)
+    transpilePackages: ['react-speech-recognition'],
 }
 
 export default nextConfig
